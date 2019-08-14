6eeaf5d344c3bda01b6442e50a8f6afea2fbf829
pankajshanee <shanees85@gmail.com>
2019-08-14 13:47:10 +0530

    # WARNING: head commit changed in the meantime
    add() method push

--- MavenFirstApp/src/main/java/TestApp.java
+++ MavenFirstApp/src/main/java/TestApp.java
@@ -5,7 +5,25 @@ public class TestApp {
 		// TODO Auto-generated method stub
 			System.out.println("Hello World");
 			
+			TestApp objTestApp = new TestApp();
+			
+			System.out.println("My name is " + objTestApp.fullName("pankaj", "shandilya"));
+			
 			System.out.println("Game about to start");
+	}
+	
+	public String fullName(String firstName, String lastName){
+		
+		String fullName = "";
+		
+		if(!("".equals(firstName)))
+			fullName = firstName;
+		
+		if(!("".equals(lastName)))
+			fullName = fullName + " " + lastName;
+		
+		return fullName;
+		
 	}
 
 }
